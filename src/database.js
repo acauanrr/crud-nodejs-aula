2909a808d7506cfb1da7ad9ec91bdf2bdfc3a21b
acauan.ribeiro@gmail.com <acauan.ribeiro@gmail.com>
2021-09-28 11:59:22 -0400

    corrigindo conexao mongo

--- src/database.js
+++ src/database.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 
 // const MONGODB_URI = 'mongodb://localhost/node-app';
-const MONGODB_URI = 'mongodb+srv://'+process.env.MONGODB_USER+':'+process.env.MONGODB_PASSWORD+'@cluster0.kpfik.mongodb.net/'+process.env.MONGODB_DATABASE+'?retryWrites=true&w=majority';
+// const MONGODB_URI = 'mongodb+srv://'+process.env.MONGODB_USER+':'+process.env.MONGODB_PASSWORD+'@cluster0.kpfik.mongodb.net/'+process.env.MONGODB_DATABASE+'?retryWrites=true&w=majority';
+
+const MONGODB_URI = 'mongodb+srv://'+process.env.MONGODB_USER+':'+process.env.MONGODB_PASSWORD+'@'+process.env.MONGODB_HOST+'/'+process.env.MONGODB_DATABASE+'?retryWrites=true&w=majority';
 
 mongoose.connect(MONGODB_URI, {
     useUnifiedTopology: true,
