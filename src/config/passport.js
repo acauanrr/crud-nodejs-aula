36c5309cf47191ef3e25766196b41ae0f1dc0db7
acauan.ribeiro@gmail.com <acauan.ribeiro@gmail.com>
2021-09-28 11:12:53 -0400

    primeiro commit

---
+++ src/config/passport.js
@@ -0,0 +1,36 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = require('../models/User');
+
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+}, async (email, password, done)=>{
+
+    // Match email's user
+    const user = await User.findOne({email});
+    if(!user) {
+        return done(null, false, {message: 'User not found.'});
+    } else {
+        // Match Password User
+        const match = await user.matchPassword(password);
+        if(match) {
+            return done(null, user); // encontrou o usuario e senha, esta tudo ok salva session no servidor
+        } else {
+            return done(null, false, {message: 'Incorrect password.'})
+        }
+    }
+} ));
+
+// Guardar a session no servidor
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+})
+
+// Quando está logado verifica se tem autorização
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user)=>{
+        done(err, user)
+    })
+});
---
+++ src/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+// const MONGODB_URI = 'mongodb://localhost/node-app';
+const MONGODB_URI = 'mongodb+srv://'+process.env.MONGODB_USER+':'+process.env.MONGODB_PASSWORD+'@cluster0.kpfik.mongodb.net/'+process.env.MONGODB_DATABASE+'?retryWrites=true&w=majority';
+
+mongoose.connect(MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+})
+.then(db => console.log('Database is connected to', db.connection.host))
+.catch(err => console.log(err));
